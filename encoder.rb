be4ea11ded47a7dc61c9053fb71f4be0e5cd4475
wvfeders <wvfedders@frontier.com>
2017-12-05 14:17:58 -0500

    encoder and decoder cleaned up comments

--- encoder.rb
+++ encoder.rb
@@ -1,16 +1,15 @@
 puts "Do you want to encode or decode?"
-code_type = gets.chomp
+code_type = gets.chomp  #User has to input whether the program should encode or decode
 
 puts "What is the number of letters used to offset the message (i. e. 5,8,13 etc)?"
-letter_offset = Integer(gets.chomp)
+letter_offset = Integer(gets.chomp) #user has to provide the number of letters to offset to create or decipher the code
 
 if code_type == "decode"
-	letter_offset = -1 * letter_offset
+	letter_offset = -1 * letter_offset #the sign of the letter_offset value is reversed if program is decoding 
 end	
 
-puts code_type, letter_offset
-#grades = { "Jane Doe" => 10, "Jim Doe" => 6 }
-#puts grades["Jane Doe"] # => 0
+puts code_type, letter_offset #checks code_type and offset
+
 
 puts "Type your message in lower case letters without punctuation: " #code only works with lower case
 message = gets.chomp 
@@ -19,10 +18,8 @@ p split_letters.count #counts total number of letters and spaces
 count=0
 (split_letters.count).times do #loops program the number of letters and spaces
 
-letter = split_letters[count] #letter variavle contains an individual letter depenndent on the value of count
+letter = split_letters[count] #letter variable contains an individual letter dependent on the value of count
 count = count + 1
-#puts letter
-#puts split_letters.length
 
 
 hash1 = {"a" => 1, "b" => 2, "c" =>3, "d" => 4, "e" => 5, "f" => 6, "g" =>7, "h" => 8,
@@ -34,21 +31,16 @@ hash1 = {"a" => 1, "b" => 2, "c" =>3, "d" => 4, "e" => 5, "f" => 6, "g" =>7, "h"
 letter_number = hash1[letter] #Letter number contains the number of each letter in the message 
 
 if letter_number == 27
-	then coded_letter = 27 # letter_number 27 refers to the space and is maintained as such
+	then coded_letter_number = 27 # letter_number 27 refers to the space and is maintained as such
 elsif	
-coded_letter = (letter_number + letter_offset)%26 #adds 13 to letternumber to encode the letter NOTE that any positive number could be added
-#if coded_letter > 26 
-#	coded_letter = coded_letter %26 #returns the remainder if coded letter is >26
-#elsif coded_letter < 0
-#	coded_letter = coded_letter%26
-#end
+	coded_letter_number = (letter_number + letter_offset)%26 #adds offset to letter number. The %26 adjusts letter number for negative values or values above 26
 end
-if coded_letter == 0 then coded_letter = 26
+if coded_letter_number == 0 then coded_letter = 26 #adjusts for instances when number is 0 
 end
 
 hash3 = hash1.invert #hash3 inverts hash1 so we can extract a letter from a number input
 
-encoded_letter = hash3[coded_letter]
+encoded_letter = hash3[coded_letter_number] #retrieves a letter from hash3 depending on the value of coded_letter_number 
 print letter, " ", encoded_letter #prints initial message and spaces and encoded letter vertically
 puts
 end
