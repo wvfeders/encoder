624cfb11af2889ffe6545b8a797b988ca3cd904a
wvfeders <wvfedders@frontier.com>
2017-12-06 17:47:10 -0500

    removed a but for coded letter == 0

--- encoder.rb
+++ encoder.rb
@@ -19,10 +19,10 @@ count=0
 (split_letters.count).times do #loops program the number of letters and spaces
 
 letter = split_letters[count] #letter variable contains an individual letter dependent on the value of count
-count = count + 1
+count = count + 1 #increments the counter
 
 
-hash1 = {"a" => 1, "b" => 2, "c" =>3, "d" => 4, "e" => 5, "f" => 6, "g" =>7, "h" => 8,
+hash1 = {"a" => 1, "b" => 2, "c" =>3, "d" => 4, "e" => 5, "f" => 6, "g" => 7, "h" => 8,
 "i" => 9, "j" => 10, "k" =>11, "l" => 12, "m" => 13, "n" => 14, "o" =>15, "p" => 16,
 "q" => 17, "r" => 18, "s" =>19, "t" => 20, "u" => 21, "v" => 22, "w" =>23, "x" => 24,
 "y" => 25, "z" => 26, " " => 27} #establishes a hash relating a, b c with 1, 2, 3
@@ -32,16 +32,17 @@ letter_number = hash1[letter] #Letter number contains the number of each letter 
 
 if letter_number == 27
 	then coded_letter_number = 27 # letter_number 27 refers to the space and is maintained as such
-elsif	
+else	
 	coded_letter_number = (letter_number + letter_offset)%26 #adds offset to letter number. The %26 adjusts letter number for negative values or values above 26
 end
-if coded_letter_number == 0 then coded_letter = 26 #adjusts for instances when number is 0 
+
+if coded_letter_number == 0 then coded_letter_number = 26 #adjusts for instances when number is 0 
 end
 
 hash3 = hash1.invert #hash3 inverts hash1 so we can extract a letter from a number input
 
 encoded_letter = hash3[coded_letter_number] #retrieves a letter from hash3 depending on the value of coded_letter_number 
-print letter, " ", encoded_letter #prints initial message and spaces and encoded letter vertically
+print letter, " ", encoded_letter, " " #prints initial message and spaces and encoded letter vertically
 puts
 end
 
